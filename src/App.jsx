35c0cb949880d9ea9473bb430f7666a5a4179525
Kushal J <jkushal208@gmail.com>
2025-08-07 14:02:39 +0530

    intialize

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <main>
+      
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <h1>Discover <span>Movies</span> You’ll Love Instantly</h1>
+      </div>
+
+   </main>
+   
+
+    
+  )
+}
+
+export default App
