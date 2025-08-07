c484f625f065403923737070f8431ad45d0df40f
Kushal J <jkushal208@gmail.com>
2025-08-07 17:45:43 +0530

    fetch data from tmdb api based on popularity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
+
+
+const API_BASE_URL= 'https://api.themoviedb.org/3';
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [searchvalue, setsearchvalue] = useState('')
+  const [errormessage, seterrormessage] = useState('')
+  const [moviedata, setmoviedata] = useState([])
+  const [isloading, setisloading] = useState(false)
 
+  const fetchmoviedata=async ()=>{
+    setisloading(true)
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+     if(!response.ok){
+      throw new Error('Error fetching movies');
+     }
+      const data = await response.json();
+      setmoviedata(data.results)
+      console.log(data);
+      setisloading(false)
+    } catch (error) {
+      seterrormessage('Error fetching movies. Try again');
+      console.error('Error fetching movies:', error);
+    }finally{
+      setisloading(false)
+    }
+  }
+  
+  useEffect(()=>{
+
+    fetchmoviedata()
+
+  },[])
   
 
   return (
@@ -19,9 +59,18 @@ const App = () => {
         <img src="./hero-img.png" alt="hero-image" />
         <h1>Discover <span className='text-gradient'>Movies</span> You’ll Love Instantly</h1>
 
-       </header>
        <Search searchvalue={searchvalue} setsearchvalue={setsearchvalue}/>
-        <h1>{searchvalue}</h1>
+       </header>
+
+       {isloading?<p className='text-red-600 text-2xl'>Loading...</p>:errormessage?<p className='text-red-500'>{errormessage}</p>:(
+         moviedata.map((movie)=>(
+          <h1 className='text-xl'>{movie.title}</h1>
+        ))
+       )}
+
+       
+
+      
       </div>
 
    </main>
